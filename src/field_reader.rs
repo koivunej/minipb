d476b6d4124ffb979ae6e1824982c3d2576bb24a
Joonas Koivunen <joonas.koivunen@gmail.com>
2020-05-26 18:16:31 +0300

    add tests for fieldreader

--- src/field_reader.rs
+++ src/field_reader.rs
@@ -69,3 +69,59 @@ impl FieldReader {
         Ok(Ok(ReadField { consumed, field }))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::io::{Cursor, Seek, SeekFrom};
+    use crate::{FieldValue, Status};
+    use super::FieldReader;
+
+    #[test]
+    fn read_basic_fields() {
+        let expected = &[
+            (FieldValue::Varint(42), 2 /* bytes consumed */, 0 /* field len */),
+            (FieldValue::Varint(242), 3, 0),
+            (FieldValue::Varint(22242), 4, 0),
+            (FieldValue::Varint(2097153), 5, 0),
+            (FieldValue::Varint(268435457), 6, 0),
+            (FieldValue::Varint(34359738369), 7, 0),
+            (FieldValue::Varint(4398046511105), 8, 0),
+            (FieldValue::Varint(562949953421313), 9, 0),
+            (FieldValue::Varint(u64::max_value()), 11, 0),
+            (FieldValue::Fixed64(1), 9, 0),
+            (FieldValue::Fixed32(1), 5, 0),
+            (FieldValue::DataLength(1), 2, 1),
+            (FieldValue::DataLength(242), 3, 242),
+            (FieldValue::DataLength(22242), 4, 22242),
+            (FieldValue::DataLength(u32::max_value()), 6, u32::max_value() as usize),
+            // longer fields are not supported
+        ];
+
+        let mut buffer = Vec::new();
+
+        for (field, expected_consumed, expected_field_len) in expected {
+
+            let mut cursor = std::io::Cursor::new(&mut buffer);
+            field.output_with_field_id(1, &mut cursor).unwrap();
+
+            cursor.seek(SeekFrom::Start(0)).unwrap();
+
+            let mut fr = FieldReader::default();
+
+            let read_field = fr.next(&buffer[..]).unwrap().unwrap();
+            assert_eq!(read_field.consumed, *expected_consumed);
+            assert_eq!(read_field.field_id(), 1);
+            assert_eq!(read_field.field_len(), *expected_field_len);
+
+            // select non-empty slices but incomplete starting from the beginning to make sure that
+            // all those report back NeedMoreBytes
+            for end_byte in 1..(buffer.len() - 1) {
+
+                let need_more = fr.next(&buffer[..end_byte]).unwrap().unwrap_err();
+                assert!(matches!(need_more, Status::NeedMoreBytes));
+            }
+
+            buffer.clear();
+        }
+    }
+}
